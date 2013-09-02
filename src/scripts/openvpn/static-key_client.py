762ba85ee8bcc8e69ec42a74988aee224e1a688d
Nick Daly <Nick.M.Daly@gmail.com>
2013-09-02 14:05:34 -0500

    Correct script header.

--- src/scripts/openvpn/static-key_client.py
+++ src/scripts/openvpn/static-key_client.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python # -*- mode: python; mode: auto-fill; fill-column: 80; -*-
+#! /usr/bin/env python
+# -*- mode: python; mode: auto-fill; fill-column: 80; -*-
 
 """Automatically configure OpenVPN tunnels from a known host.
 
