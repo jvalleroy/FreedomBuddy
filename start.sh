9a080b8a5552a3553b5f03183d66549e564666b8
Nick Daly <Nick.M.Daly@gmail.com>
2015-03-15 09:27:40 -0500

    Simplify start.sh by getting rid of the individual pieces.
    If you're trying to debug it, you can run each line individually.
    Let's simplify things by getting rid of meaningless numeric arguments
    that exist to make it harder to do the thing you want to do, in most
    cases.

--- start.sh
+++ start.sh
@@ -1,56 +1,24 @@
 #! /bin/sh
 # -*- mode: sh; mode: auto-fill; fill-column: 80 -*-
 
-PYTHONPATH=../:$PYTHONPATH
-PYTHONPATH=build/gnupg:$PYTHONPATH
-PYTHONPATH=build/plinth:$PYTHONPATH
-PYTHONPATH=build/bjsonrpc:$PYTHONPATH
+PYTHONPATH=../:.:$PYTHONPATH
 export PYTHONPATH
 
+# do everything in multiple terminals.
 
-if [ "$1" -eq "0" ]
-then
-    # initial run
-    python src/santiago_run.py "$@" &
+# start fbuddy + cli client
+x-terminal-emulator -e "python src/santiago_run.py" &
 
-elif [ "$1" -eq "1" ]
-then
-    # start santiago
-    python src/santiago_run.py "$@"
+# start https client
+x-terminal-emulator -e \
+"python src/connectors/https/controller.py --listen" &
+x-terminal-emulator -e \
+"python src/connectors/https/controller.py --monitor" &
 
-elif [ "$1" -eq "2" ]
-then
-    # supply the cli controller any action.
-    python src/connectors/cli/controller.py "$@"
+# start a browser for the monitor
+x-terminal-emulator -e \
+"sleep 5 && x-www-browser https://127.0.0.1:8081/freedombuddy" &
 
-elif [ "$1" -eq "3" ]
-then
-    # supply the cli controller any action.
-    python src/connectors/https/controller.py "--listen"
-
-elif [ "$1" -eq "4" ]
-then
-    # give the https controller any action.
-    python src/connectors/https/controller.py "--monitor"
-
-elif [ "$1" -eq "5" ]
-then
-    # do EVERYTHING in multiple terminals.
-
-    # start fbuddy + cli client
-    x-terminal-emulator -e "python src/santiago_run.py" &
-
-    # start https client
-    x-terminal-emulator -e \
-	"python src/connectors/https/controller.py --listen" &
-    x-terminal-emulator -e \
-	"python src/connectors/https/controller.py --monitor" &
-
-    # start a browser for the monitor
-    x-terminal-emulator -e \
-	"sleep 5 && x-www-browser https://127.0.0.1:8081/freedombuddy" &
-
-    echo "Press return to quit."
-    read X
-    killall python
-fi
+echo "Press return to quit."
+read X
+kill `ps | grep term | awk '{ print $1 }'`
