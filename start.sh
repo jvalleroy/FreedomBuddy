94230c5d1a869c38f619a93d0a47de2cfc920d3b
Nick Daly <Nick.M.Daly@gmail.com>
2015-03-23 13:56:06 -0500

    Record and kill PIDs more effectively.

--- start.sh
+++ start.sh
@@ -1,24 +1,34 @@
 #! /bin/sh
 # -*- mode: sh; mode: auto-fill; fill-column: 80 -*-
+#
+# do everything in multiple terminals.
 
 PYTHONPATH=../:.:$PYTHONPATH
 export PYTHONPATH
 
-# do everything in multiple terminals.
-
 # start fbuddy + cli client
-x-terminal-emulator -e "python src/santiago_run.py" &
+x-terminal-emulator -e "python src/santiago_run.py $@" &
+echo $! >> santiago.pid
+
 
 # start https client
 x-terminal-emulator -e \
 "python src/connectors/https/controller.py --listen" &
+echo $! >> santiago.pid
+
 x-terminal-emulator -e \
 "python src/connectors/https/controller.py --monitor" &
+echo $! >> santiago.pid
+
 
 # start a browser for the monitor
 x-terminal-emulator -e \
 "sleep 5 && x-www-browser https://127.0.0.1:8081/freedombuddy" &
+echo $! >> santiago.pid
 
-echo "Press return to quit."
+
+# and bail when we're done.
+echo "Stop the Santiago process to save data and then press return to quit."
 read X
-kill `ps | grep term | awk '{ print $1 }'`
+kill `cat santiago.pid` > /dev/null 2>&1
+rm santiago.pid
