0fcf9ddc4d81c9803038937392b54de29fd5f305
Nick Daly <Nick.M.Daly@gmail.com>
2016-01-03 11:39:18 -0600

    Add `--stop` and `--verbose` options.
    Stop: Don't start processes, kill the ones in santiago.pid.
    Verbose: Display pids as they're killed.

--- start.sh
+++ start.sh
@@ -5,23 +5,71 @@
 
 PYTHONPATH=.:$PYTHONPATH
 export PYTHONPATH
+verbose=0
 
 #
 # kill any running servers/clients when exiting.
 cleanup() {
-    python src/connectors/cli/controller.py --stop
+    if [ "$verbose" = "1" ]
+    then
+        python src/connectors/cli/controller.py --stop
+    else
+        python src/connectors/cli/controller.py --stop 2>/dev/null
+    fi
 
     # don't die if we can't kill everything.
     for pid in $cliClient $httpsClient $httpsMonitor $browser
     do
-        kill $pid
-        if [ $? -eq 0 ]
+        if [ "$verbose" = "1" ]
         then
+            echo -n "Killing ${pid}... "
+        fi
+
+        ps $pid | grep $pid 2>&1 > /dev/null
+
+        if [ "$?" = "0" ]
+        then
+            kill $pid
+        else
+            true
+        fi
+
+        if [ "$?" = "0" ]
+        then
+            if [ "$verbose" = "1" ]
+            then
+                echo "Done."
+            fi
             sed -i "/^$pid/d" santiago.pid
         fi
     done
 }
 trap cleanup EXIT
+
+loadPids() {
+    cliClient=`grep "cli client" santiago.pid | awk '{ print $1 }'`
+    httpsClient=`grep "https client" santiago.pid | awk '{ print $1 }'`
+    httpsMonitor=`grep "https monitor" santiago.pid | awk '{ print $1 }'`
+    browser=`grep "browser" santiago.pid | awk '{ print $1 }'`
+}
+
+for arg in $@
+do
+    if [ "$arg" = "--verbose" ]
+    then
+        verbose=1
+    fi
+    if [ "$arg" = "--stop" ]
+    then
+        stop=1
+    fi
+done
+
+if [ "$stop" = "1" ]
+then
+    loadPids
+    exit
+fi
 
 #
 # start fbuddy + cli client
